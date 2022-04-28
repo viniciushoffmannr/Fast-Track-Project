eebf2528137d03a431d757e9229bf45f12a462fb
viniciushoffmannr <95242473+viniciushoffmannr@users.noreply.github.com>
2022-04-28 15:30:51 -0300

    estilizacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import rickFace from "../../assets/rickface.png";
+import rickFace from "../../assets/Rick_SVG.svg";
 import "./style.css";
 
 export default function Header() {
@@ -9,6 +9,7 @@ export default function Header() {
       <header className="container">
         <Link to="/">
           <img className="logo" src={rickFace} alt="Rosto do Rick" />
+          <h1 className="principalName">TiwtRick</h1>
         </Link>
       </header>
     </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img from "../../assets/Rick_SVG.svg";
+import css from "./style.css";
 
 export default function Home() {
-  return <div>Home!</div>;
+  return (
+    <div className="home">
+      <nav class="">
+        <div class="container-fluid">
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-light" type="submit">
+              Search
+            </button>
+          </form>
+          <p>Search for a character. Like Rick...</p>
+        </div>
+      </nav>
+
+      <div className="cardBox">
+        <div class="card">
+          <img src={img} class="cardImg card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img src={img} class="cardImg card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img src={img} class="cardImg card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
